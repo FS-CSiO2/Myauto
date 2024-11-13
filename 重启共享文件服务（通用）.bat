83dfd931f638747d34d4f9f23bd9be538a4d34d0
FS-CSiO2 <1127393924@qq.com>
2024-11-13 14:28:41 +0800

    以bat去使用powshell的重启workstation服务
    powshell开机后第一次使用会用点卡，但是目前看来他的泛用性是要比直接用bat脚本实现要高

---
+++ 重启共享文件服务（通用）.bat
@@ -0,0 +1,29 @@
+@echo off
+setlocal DisableDelayedExpansion
+set "batchPath=%~0"
+for %%k in (%0) do set batchName=%%~nk
+set "vbsGetPrivileges=%temp%\MYSQL_INIT_getPriv_%batchName%.vbs"
+setlocal EnableDelayedExpansion
+:checkPrivileges
+NET FILE 1>NUL 2>NUL
+if '%errorlevel%' == '0' ( goto gotPrivileges ) else ( goto getPrivileges )
+:getPrivileges
+if '%1'=='ELEV' (echo ELEV & shift /1 & goto gotPrivileges)
+ECHO Set UAC = CreateObject^("Shell.Application"^) > "%vbsGetPrivileges%"
+ECHO args = "ELEV " >> "%vbsGetPrivileges%"
+ECHO For Each strArg in WScript.Arguments >> "%vbsGetPrivileges%"
+ECHO args = args ^& strArg ^& " "  >> "%vbsGetPrivileges%"
+ECHO Next >> "%vbsGetPrivileges%"
+ECHO UAC.ShellExecute "!batchPath!", args, "", "runas", 1 >> "%vbsGetPrivileges%"
+"%SystemRoot%\System32\WScript.exe" "%vbsGetPrivileges%" %*
+exit /B
+:gotPrivileges
+setlocal & pushd .
+cd /d %~dp0
+if '%1'=='ELEV' (del "%vbsGetPrivileges%" 1>nul 2>nul  &  shift /1)
+cls
+@echo off
+echo 尝试重启workstation服务
+powershell -Command "Get-Service -Name 'Workstation' | Restart-Service -Force"
+echo 重启workstation服务结束  
+pause
